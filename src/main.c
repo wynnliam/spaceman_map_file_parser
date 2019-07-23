b95c10afca16ce2e5bc870c3b6ef9c1a25459511
Liam Wynn <wynnliam@pdx.edu>
2019-07-23 14:19:37 -0700

    Updated texlist_data and texture_list functions to use mapdef_id, num_floor_ceils, and num_walls

--- src/main.c
+++ src/main.c
@@ -6,23 +6,24 @@
 #include "./parser/thing_list.h"
 
 int main() {
+	// TODO: Standardize texture list
 	struct token_list* tokens = get_tokens("./bin/c02.sqm");
 	struct recipe_list* map_tree = build_map_tree(tokens);
 	struct thing_list* thing_list = build_thing_list_from_map_tree(map_tree);
-
-	print_thing_list(thing_list);
-	clean_thing_list(thing_list);
-	//struct texture_list* texture_list = construct_texture_list();
+	struct texture_list* texture_list = construct_texture_list();
+	
+	build_texture_list_from_map_tree(map_tree, texture_list);
 
 	//print_recipe_list(map_tree, 0);
 
-	//build_texture_list_from_map_tree(map_tree, texture_list);
-	//print_texture_list(texture_list);
+	print_texture_list(texture_list);
+	print_thing_list(thing_list);
 
 	clean_token_list(tokens);
 	clean_recipe_list(map_tree);
-	//clear_texture_list(texture_list);
-	//free(texture_list);
+	clean_thing_list(thing_list);
+	clear_texture_list(texture_list);
+	free(texture_list);
 
 	/*struct thinglist_data* thing_data = construct_thinglist_data(NULL, 0, 1120, 640, 90);
 	struct thinglist_data* thing_data_2 = construct_thinglist_data("www", 0, 1120, 640, 90);
--- src/parser/texture_list.c
+++ src/parser/texture_list.c
@@ -28,6 +28,7 @@ struct texlist_data* construct_texlist_data(const char* tex_0, const char* tex_1
 	}
 
 	result->is_floor_ceil_pair = is_floor_ceil_pair;
+	result->mapdef_id = 0;
 
 	return result;
 }
@@ -49,7 +50,7 @@ int texlist_data_equals(struct texlist_data* a, struct texlist_data* b) {
 	else
 		tex_1_cmp = strcmp(a->tex_1, b->tex_1) == 0;
 
-	return tex_0_cmp && tex_1_cmp && (a->is_floor_ceil_pair == b->is_floor_ceil_pair);
+	return tex_0_cmp && tex_1_cmp && a->is_floor_ceil_pair == b->is_floor_ceil_pair && a->mapdef_id == b->mapdef_id;
 }
 
 void clean_textlist_data(struct texlist_data* to_clean) {
@@ -73,6 +74,7 @@ void print_texlist_data(struct texlist_data* data) {
 	if(!data)
 		return;
 
+	printf("mapdef_id: %d\n", data->mapdef_id);
 	printf("tex_0: %s\n", data->tex_0 ? data->tex_0 : "0");
 	printf("tex_1: %s\n", data->tex_1 ? data->tex_1 : "0");
 	printf("Is floor/ceiling pair: %s\n", data->is_floor_ceil_pair ? "True" : "False");
@@ -95,12 +97,24 @@ struct texture_list* construct_texture_list() {
 
 	result->head = NULL;
 
+	result->num_floor_ceils = 0;
+	result->num_walls = 0;
+
 	return result;
 }
 
 int insert_texture_into_texture_list(struct texture_list* list, struct texlist_data* data) {
 	if(!data)
 		return 0;
+
+	// Must set the id for data and counter
+	if(data->is_floor_ceil_pair) {
+		data->mapdef_id = list->num_floor_ceils;
+		list->num_floor_ceils += 1;
+	} else {
+		data->mapdef_id = list->num_walls + 100;
+		list->num_walls += 1;
+	}
 
 	struct texlist_node* temp = construct_texlist_node(data);
 	temp->next = list->head;
