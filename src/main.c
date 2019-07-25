9579434ab5398bc10be151cfafc294f64d14788b
Liam Wynn <wynnliam@pdx.edu>
2019-07-25 12:26:35 -0700

    Implemented construct_intermediate_mapdef procedure

--- src/main.c
+++ src/main.c
@@ -3,36 +3,32 @@
 
 #include "./map_loading/tokenizer/tokenizer.h"
 #include "./map_loading/parser/parser.h"
-#include "./map_loading/ir/texture_list.h"
-#include "./map_loading/ir/thing_list.h"
-#include "./map_loading/ir/component_list.h"
+#include "./map_loading/ir/intermediate_mapdef.h"
 
 int main() {
 	// TODO: Standardize texture list
 	struct token_list* tokens = get_tokens("./bin/c02.sqm");
 	struct recipe_list* map_tree = build_map_tree(tokens);
-	struct thing_list* thing_list = build_thing_list_from_map_tree(map_tree);
-	struct texture_list* texture_list = construct_texture_list();
-	struct component_list* components;
-	
-	build_texture_list_from_map_tree(map_tree, texture_list);
 
-	components = build_component_list_from_recipe_list_and_texture_list(map_tree, texture_list);
+	struct intermediate_mapdef* intermediate_mapdef = construct_intermediate_mapdef(map_tree);
 
-	print_recipe_list(map_tree, 0);
+	print_token_list(tokens);
 	printf("\n");
+	print_recipe_list(map_tree, 0);
+	/*printf("\n");
 	print_texture_list(texture_list);
 	printf("\n");
 	print_thing_list(thing_list);
 	printf("\n");
-	print_component_list(components);
+	print_component_list(components);*/
 
 	clean_token_list(tokens);
 	clean_recipe_list(map_tree);
-	clean_thing_list(thing_list);
+
+	/*clean_thing_list(thing_list);
 	clean_component_list(components);
 
 	clear_texture_list(texture_list);
-	free(texture_list);
+	free(texture_list);*/
 	return 0;
 }
--- src/map_loading/ir/intermediate_mapdef.c
+++ src/map_loading/ir/intermediate_mapdef.c
@@ -10,7 +10,12 @@ struct intermediate_mapdef* construct_intermediate_mapdef(struct recipe_list* ma
 
 	struct intermediate_mapdef* result = (struct intermediate_mapdef*)malloc(sizeof(struct intermediate_mapdef));
 
-	// ...
+	result->textures = construct_texture_list();
+	build_texture_list_from_map_tree(map_tree, result->textures);
+
+	result->things = build_thing_list_from_map_tree(map_tree);
+
+	result->components = build_component_list_from_recipe_list_and_texture_list(map_tree, result->textures);
 
 	return result;
 }
